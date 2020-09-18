540dfb55d50f2e143150b1b238f6cb2e93a41ebc
ltstaffel <cpadiak@protonmail.com>
2020-09-18 17:49:40 -0400

    final

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # all the apt packages that don't need special repos
 apt update -y
-apt install $(cat apt/packages.txt) -y
+apt install $(cat ./apt/packages.txt) -y
 
 # get keys for special repos
 ./apt/keys.sh
@@ -10,19 +10,19 @@ apt install $(cat apt/packages.txt) -y
 cp apt/sources.list /etc/apt/sources.list
 # install from special repos
 apt update -y
-apt install $(cat apt/pkgsfromrepos.txt) -y
+apt install $(cat ./apt/pkgsfromrepos.txt) -y
 
 # misc
 ./misc/docker-compose.sh
 
 # pip
-pip3 install -r pip/requirements.txt
+pip3 install -r ./pip/requirements.txt
 
 apt update -y
 apt full-upgrade -y
 apt autoremove -y
 
 # shell
-cp shell/mine.zsh-theme ~/.oh-my-zsh/themes
-cp shell/.zshrc ~/.zshrc
-cp shell/.vimrc ~/.vimrc
+cp ./shell/mine.zsh-theme ~/.oh-my-zsh/themes
+cp ./shell/.zshrc ~/.zshrc
+cp ./shell/.vimrc ~/.vimrc
