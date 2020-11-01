724696ad90e4d295e0d98c91931ab76e8b00c825
larkwiot <ltfragor@gmail.com>
2020-11-01 16:02:51 -0500

    some confs

---
+++ shell/config.fish
@@ -0,0 +1,16 @@
+set -x PATH $PATH:/home/me/.local/bin
+
+alias ls="exa"
+alias ll="exa -l"
+alias la="exa -la"
+alias lt="exa --tree"
+
+alias cat="bat"
+
+alias dc="docker-compose"
+alias dclogs="docker-compose logs"
+alias dcupd="docker-compose up -d"
+alias dcdn="docker-compose down"
+alias dcrestart="docker-compose restart"
+
+alias ugly="ugrep -nrIQZ -e \"\""
