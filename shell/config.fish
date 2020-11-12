f71cb1e2cb68c4b952a578b3ac2ff5757f398acb
larkwiot <ltfragor@gmail.com>
2020-11-11 21:47:56 -0500

    tmux conf updates

--- shell/config.fish
+++ shell/config.fish
@@ -2,6 +2,9 @@ set -x PATH $PATH:/home/me/.local/bin
 set -x PATH $PATH:/home/me/lopt/pycharm-community-2020.2.3/bin
 set -x PATH $PATH:/home/me/lopt/clion-2020.2.4/bin
 
+set -x EDITOR /bedrock/cross/bin/micro
+set -x PAGER /bedrock/cross/bin/most
+
 alias ls="exa -l"
 alias ll="exa -l"
 alias la="exa -la"
@@ -25,6 +28,11 @@ alias wget="wget2"
 
 alias cp="rsync -v --progress"
 
+alias leave="pkill alacritty"
+
+alias tohex="begin; echo 'obase=16;ibase=10'; cat -; end | bc"
+alias todec="begin; echo 'obase=10;ibase=16'; cat -; end | bc"
+
 if not [ "$TMUX" ]
-    exec tmux && exit
+    tmux attach; or tmux
 end
